85c6f7cb18235800315d1bd304f36869f74c68a7
sangwoo <kajer102@gmail.com>
2023-12-20 15:36:59 +0900

    과제1

---
+++ lib/23_12_20/exam/00_exam1.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Birth1(),
+    );
+  }
+}
+
+class Birth1 extends StatelessWidget {
+  const Birth1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Happy',
+                  style: TextStyle(fontSize: 80),
+                ),
+                Text(
+                  'Birthday',
+                  style: TextStyle(fontSize: 80),
+                ),
+                Text(
+                  'Sam!',
+                  style: TextStyle(fontSize: 80),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'From Emma',
+                      style: TextStyle(fontSize: 32),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
