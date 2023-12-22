71c81490e718ded3539b7bf9b9e063771c7c9d7b
sangwoo <kajer102@gmail.com>
2023-12-22 14:41:32 +0900

    이미지 나열

--- lib/23_12_22/exam1/main.dart
+++ lib/23_12_22/exam1/main.dart
@@ -21,8 +21,56 @@ class Starbucks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('스타벅스'),
+      body: ListView(
+        children: [
+          Image.asset('assets/starbucks-image/starbucks_01.png'),
+          Container(
+            child: const Row(
+              children: [
+                Column(),
+                Text('0 / 5 asd'),
+              ],
+            ),
+          ),
+          Image.asset(
+              'assets/starbucks-image/01_01_2023_winter_e-frequency.png'),
+          Image.asset('assets/starbucks-image/02_01_siren_order.png'),
+          Container(
+            child: const Row(
+              children: [
+                Column(),
+                Text('0 / 5 asd'),
+              ],
+            ),
+          ),
+          Image.asset('assets/starbucks-image/03_01_chrismas_event.png'),
+          Container(
+            child: const Row(
+              children: [
+                Column(),
+                Text('What\'s New'),
+              ],
+            ),
+          ),
+          Image.asset('assets/starbucks-image/05_01_card.png'),
+          Image.asset('assets/starbucks-image/05_02_card.png'),
+          Container(
+            child: const Row(
+              children: [
+                Column(),
+                Text('누구누구님을 위한 추천 메뉴'),
+              ],
+            ),
+          ),
+          Image.asset('assets/starbucks-image/06_01_card.png'),
+          Image.asset('assets/starbucks-image/06_02_card.png'),
+          Image.asset('assets/starbucks-image/06_03_card.png'),
+          Image.asset('assets/starbucks-image/06_04_card.png'),
+          Image.asset('assets/starbucks-image/06_05_card.png'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
