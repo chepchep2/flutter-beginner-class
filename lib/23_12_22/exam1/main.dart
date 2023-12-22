82bf5e112e4be7c629d16fe52f5ebc6b0cda89de
sangwoo <kajer102@gmail.com>
2023-12-22 15:44:57 +0900

    google map 시작

--- lib/23_12_22/exam1/main.dart
+++ lib/23_12_22/exam1/main.dart
@@ -9,68 +9,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Starbucks(),
     );
   }
 }
 
-class Starbucks extends StatelessWidget {
-  const Starbucks({super.key});
+class Starbucks extends StatefulWidget {
+  bool extended = false;
 
+  Starbucks({super.key});
+
+  @override
+  State<Starbucks> createState() => _StarbucksState();
+}
+
+class _StarbucksState extends State<Starbucks> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Image.asset('assets/starbucks-image/starbucks_01.png'),
-          Container(
-            child: const Row(
-              children: [
-                Column(),
-                Text('0 / 5 asd'),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Image.asset('assets/starbucks-image/starbucks_01.png'),
+            Container(
+              child: const Row(
+                children: [
+                  Column(),
+                  Text('0 / 5 asd'),
+                ],
+              ),
             ),
-          ),
-          Image.asset(
-              'assets/starbucks-image/01_01_2023_winter_e-frequency.png'),
-          Image.asset('assets/starbucks-image/02_01_siren_order.png'),
-          Container(
-            child: const Row(
-              children: [
-                Column(),
-                Text('0 / 5 asd'),
-              ],
+            Image.asset(
+                'assets/starbucks-image/01_01_2023_winter_e-frequency.png'),
+            Image.asset('assets/starbucks-image/02_01_siren_order.png'),
+            Container(
+              child: const Row(
+                children: [
+                  Column(),
+                  Text('0 / 5 asd'),
+                ],
+              ),
             ),
-          ),
-          Image.asset('assets/starbucks-image/03_01_chrismas_event.png'),
-          Container(
-            child: const Row(
-              children: [
-                Column(),
-                Text('What\'s New'),
-              ],
+            Image.asset('assets/starbucks-image/03_01_chrismas_event.png'),
+            Container(
+              child: const Row(
+                children: [
+                  Column(),
+                  Text('What\'s New'),
+                ],
+              ),
             ),
-          ),
-          Image.asset('assets/starbucks-image/05_01_card.png'),
-          Image.asset('assets/starbucks-image/05_02_card.png'),
-          Container(
-            child: const Row(
-              children: [
-                Column(),
-                Text('누구누구님을 위한 추천 메뉴'),
-              ],
+            Image.asset('assets/starbucks-image/05_01_card.png'),
+            Image.asset('assets/starbucks-image/05_02_card.png'),
+            Container(
+              child: const Row(
+                children: [
+                  Column(),
+                  Text('누구누구님을 위한 추천 메뉴'),
+                ],
+              ),
             ),
-          ),
-          Image.asset('assets/starbucks-image/06_01_card.png'),
-          Image.asset('assets/starbucks-image/06_02_card.png'),
-          Image.asset('assets/starbucks-image/06_03_card.png'),
-          Image.asset('assets/starbucks-image/06_04_card.png'),
-          Image.asset('assets/starbucks-image/06_05_card.png'),
-        ],
+            Image.asset('assets/starbucks-image/06_01_card.png'),
+            Image.asset('assets/starbucks-image/06_02_card.png'),
+            Image.asset('assets/starbucks-image/06_03_card.png'),
+            Image.asset('assets/starbucks-image/06_04_card.png'),
+            Image.asset('assets/starbucks-image/06_05_card.png'),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
+        label: const Text('delivery'),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
