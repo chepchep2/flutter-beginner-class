6d4b68da37ef2169157cbc0d7aec0d409e7f249a
sangwoo <kajer102@gmail.com>
2023-12-25 20:23:16 +0900

    TodoAdapter 생성

--- lib/23_12_25/todo.dart
+++ lib/23_12_25/todo.dart
@@ -2,6 +2,8 @@
 
 import 'package:hive/hive.dart';
 
+part 'todo.g.dart';
+
 @HiveType(typeId: 0)
 class Todo extends HiveObject {
   @HiveField(0)
--- lib/23_12_25/todolist.dart
+++ lib/23_12_25/todolist.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_beginner_class/23_12_25/list_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
