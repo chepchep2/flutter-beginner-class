920300ab512bd68692eca203209707ffba005735
sangwoo <kajer102@gmail.com>
2023-12-25 12:13:29 +0900

    Todo 작성 페이지 완성

--- lib/23_12_25/list_screen.dart
+++ lib/23_12_25/list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beginner_class/23_12_25/create_screen.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
@@ -9,8 +10,26 @@ class ListScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Todo 리스트'),
       ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('타이틀 1'),
+            subtitle: Text('서브 타이틀 1'),
+          ),
+          ListTile(
+            title: Text('타이틀 2'),
+            subtitle: Text('서브 타이틀 2'),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CreateScreen(),
+              ));
+        },
         child: const Icon(Icons.add),
       ),
     );
