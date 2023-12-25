3a245ccb5b3f2935d26e69d2b2e30be544a0fd36
sangwoo <kajer102@gmail.com>
2023-12-25 20:15:45 +0900

    hive 사전 작업

--- lib/23_12_25/todo.dart
+++ lib/23_12_25/todo.dart
@@ -4,7 +4,11 @@ import 'package:hive/hive.dart';
 
 @HiveType(typeId: 0)
 class Todo extends HiveObject {
+  @HiveField(0)
+  int? id;
+  @HiveField(1)
   String title;
+  @HiveField(2)
   int dateTime;
 
   Todo({
