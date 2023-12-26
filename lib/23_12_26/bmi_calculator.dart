31590aaf598fa4a011f2024dbb32427d1784b56d
sangwoo <kajer102@gmail.com>
2023-12-26 15:58:04 +0900

    bmi 완성

--- lib/23_12_26/bmi_calculator.dart
+++ lib/23_12_26/bmi_calculator.dart
@@ -26,6 +26,16 @@ class _MainScreenState extends State<MainScreen> {
   final _formKey = GlobalKey<FormState>();
   // form의 상태를 가지고 있다.
 
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,6 +50,7 @@ class _MainScreenState extends State<MainScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               TextFormField(
+                controller: _heightController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(), hintText: '키'),
                 keyboardType: TextInputType.number,
@@ -52,6 +63,7 @@ class _MainScreenState extends State<MainScreen> {
               ),
               const SizedBox(height: 8),
               TextFormField(
+                controller: _weightController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(), hintText: '몸무게'),
                 keyboardType: TextInputType.number,
@@ -65,7 +77,19 @@ class _MainScreenState extends State<MainScreen> {
               const SizedBox(height: 8),
               ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {}
+                  if (_formKey.currentState?.validate() == false) {
+                    return;
+                  }
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ResultScreen(
+                        height: double.parse(_heightController.text),
+                        weight: double.parse(_weightController.text),
+                      ),
+                    ),
+                  );
                 },
                 child: const Text('결과'),
               ),
@@ -78,10 +102,77 @@ class _MainScreenState extends State<MainScreen> {
 }
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  final double height;
+  final double weight;
+
+  const ResultScreen({
+    super.key,
+    required this.height,
+    required this.weight,
+  });
+
+  String _calcBmi(double bmi) {
+    String result = '저체중';
+
+    if (bmi >= 35) {
+      result = '고도비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과제충';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    Icon icon = const Icon(
+      Icons.sentiment_dissatisfied,
+      color: Colors.red,
+      size: 100,
+    );
+
+    if (bmi >= 23) {
+      icon = const Icon(
+        Icons.sentiment_dissatisfied_outlined,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      icon = const Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    }
+
+    return icon;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final double bmi = weight / ((height / 100) * (height / 100));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('결과'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _calcBmi(bmi),
+              style: const TextStyle(fontSize: 36),
+            ),
+            _buildIcon(bmi),
+          ],
+        ),
+      ),
+    );
   }
 }
