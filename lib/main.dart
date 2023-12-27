0dbb64c609c0481989186240c77dd63276ad4a7b
sangwoo <kajer102@gmail.com>
2023-12-27 17:38:04 +0900

    커밋 체크

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       // sd
       // ㅇㅇ
+      // ㅇㅇㅇㅇㅇㅇ
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
