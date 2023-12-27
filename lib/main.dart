891ef0a21cbdfc35ff91c309993824523d0673b6
sangwoo <kajer102@gmail.com>
2023-12-27 17:35:54 +0900

    수정 체크

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       // sd
+      // ㅇㅇ
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
